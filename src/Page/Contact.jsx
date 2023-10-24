a279c584e617c4918d9117d8a65a30602545fcb3
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-25 01:06:07 +0600

    input name

--- src/Page/Contact.jsx
+++ src/Page/Contact.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
+            <label htmlFor="">NAme</label>
             <input type="text" />
         </div>
     );
